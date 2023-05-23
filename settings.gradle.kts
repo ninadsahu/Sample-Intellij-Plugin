4b2650d935710f93afabc2eebf05e32515a0f57b
GitHub Action <action@github.com>
2023-05-23 08:46:12 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "Sample-Intellij-Plugin"
--- src/main/kotlin/com/github/ninadsahu/sampleintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/ninadsahu/sampleintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ninadsahu.sampleintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
